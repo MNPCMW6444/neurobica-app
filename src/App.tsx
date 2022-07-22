266b40c77a68c6bce26e373fd9fa6192990e416a
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-22 12:19:20 +0300

    fix deleted work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const { Sidebar, SidebarItem } = require("react-responsive-sidebar");
+
+const pages = [
+  <SidebarItem>Dashboard</SidebarItem>,
+  <SidebarItem>Profile</SidebarItem>,
+  <SidebarItem>Settings</SidebarItem>,
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar content={pages}></Sidebar>
     </div>
   );
 }
