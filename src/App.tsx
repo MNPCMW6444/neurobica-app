6c580cce867fe0c8b2ed17d55d60541fb6b880db
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-22 12:05:11 +0300

    No more ui - create-react-app and sidebar
    before Pivot
    Vite is doing problems.
    and Actually? we dont want to be small.
    after pivot

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,26 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-=======
-import "./App.css";
-const { Sidebar, SidebarItem } = require("react-responsive-sidebar");
-
-function App() {
-  const items = [
-    <SidebarItem>Dashboard</SidebarItem>,
-    <SidebarItem>Profile</SidebarItem>,
-    <SidebarItem>Settings</SidebarItem>,
-  ];
-
-  return (
-    <div className="App">
-      <Sidebar content={items}></Sidebar>
->>>>>>> Stashed changes
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
