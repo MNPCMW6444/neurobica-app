cd0a0ddd154925fc5c22cd842c193161f2477d09
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-12 00:01:09 +0300

    preparation for loginpage

--- src/components/pages/global-components/status-bar/status-components/login-status/LoginStatus.tsx
+++ src/components/pages/global-components/status-bar/status-components/login-status/LoginStatus.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./loginStatus.css";
 import useUser from "../../../../../../hooks/useUser";
+import Button from "@mui/material/Button";
+import loginButtonSx from "./loginButtonSs";
 
 export default function LoginStatus() {
   const [user] = useUser("loggedIn", {}, []);
@@ -10,7 +12,10 @@ export default function LoginStatus() {
       {user._id ? (
         "Welcome back " + user.fullname + "!"
       ) : (
-        <button>Login</button>
+        <>
+          {/* <Button sx={loginButtonSx} onClick={()=>{}}>Login</Button> */}
+          <a href="/my-account">Login</a>
+        </>
       )}
     </div>
   );
