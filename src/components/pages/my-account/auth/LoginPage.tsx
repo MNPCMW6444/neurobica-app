ecf3f6daae0758dffa3f848100297743d234c86b
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-14 01:03:43 +0300

    tes clean and if not then dirty

--- src/components/pages/my-account/auth/LoginPage.tsx
+++ src/components/pages/my-account/auth/LoginPage.tsx
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
-=======
-import LoginPageSignInSignUp from "./login-page/login-page-sign-in-sign-up/LoginPageSignInSignUp";
-
-export default function LoginPage() {
-  return (
-    <div>
-      {/*       <LoginPageLogo />
-       */}
-      <LoginPageSignInSignUp />
-      {/*  <LoginPageFields />
-      <LoginPageOptions />
-      <LoginPageSendButton />
-      <LoginPageExternalAuthButtons />
-      <LoginPagefooter />*/}
-    </div>
-  );
->>>>>>> parent of 478f80f (login page logo)
+  return <div>Yoad make the UI herer\</div>;
 }
