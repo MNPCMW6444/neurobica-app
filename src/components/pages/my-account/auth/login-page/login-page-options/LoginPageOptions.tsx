ee9959c36cf454cecc897820d56d62ca8966310e
YoadNatan <106985580+YoadNatan@users.noreply.github.com>
2022-08-13 10:54:10 +0300

    login-page-options

--- src/components/pages/my-account/auth/login-page/login-page-options/LoginPageOptions.tsx
+++ src/components/pages/my-account/auth/login-page/login-page-options/LoginPageOptions.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
 
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import"./loginPageOptions.css"
 export default function LoginPageOptions() {
   return (
-    <div>LoginPageOptions</div>
+    <div className='login-page-options'>
+      
+      <Checkbox defaultChecked 
+  sx={{
+    color: "white[800]",
+    '&.Mui-checked': {
+      color:" pink[600]",
+    },
+  }}
+/>
+<FormControlLabel control={<Checkbox defaultChecked />} label="Forgot Password?" />
+
+    </div>
   )
 }
