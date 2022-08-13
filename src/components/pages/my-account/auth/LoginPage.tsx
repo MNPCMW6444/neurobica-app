fcb3e2d78971915844670965861d2af2c1b7b8ce
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-14 01:02:01 +0300

    Update LoginPage.tsx

--- src/components/pages/my-account/auth/LoginPage.tsx
+++ src/components/pages/my-account/auth/LoginPage.tsx
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
 import React from "react";
 
 export default function LoginPage() {
   return <div>LoginPage</div>;
+=======
+import LoginPageSignInSignUp from "./login-page/login-page-sign-in-sign-up/LoginPageSignInSignUp";
+
+export default function LoginPage() {
+  return (
+    <div>
+      {/*       <LoginPageLogo />
+       */}
+      <LoginPageSignInSignUp />
+      {/*  <LoginPageFields />
+      <LoginPageOptions />
+      <LoginPageSendButton />
+      <LoginPageExternalAuthButtons />
+      <LoginPagefooter />*/}
+    </div>
+  );
+>>>>>>> parent of 478f80f (login page logo)
 }
