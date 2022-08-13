45bc5f86388066f77f9a8be63b0a7d5980237ccf
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-14 01:32:03 +0300

    Revert "sdf"
    This reverts commit ccb7617ab031ebe16f71283123adbd7bf9e6d5dc.

--- src/components/pages/my-account/auth/LoginPage.tsx
+++ src/components/pages/my-account/auth/LoginPage.tsx
@@ -1,24 +1,5 @@
-import LoginPageLogo from "./login-page/login-page-logo/LoginPageLogo";
-import LoginPageExternalAuthButtons from "./login-page/login-page-external-auth-buttons/LoginPageExternalAuthButtons";
-import LoginPageFields from "./login-page/login-page-fields/LoginPageFields";
-import LoginPagefooter from "./login-page/login-page-footer/LoginPagefooter";
-import LoginPageOptions from "./login-page/login-page-options/LoginPageOptions";
-import LoginPageSendButton from "./login-page/login-page-send-button/LoginPageSendButton";
-import LoginPageSignInSignUp from "./login-page/login-page-sign-in-sign-up/LoginPageSignInSignUp";
+import React from "react";
 
 export default function LoginPage() {
-  return (
-    <div>
- login-page-mui
-      <LoginPageLogo />
-  
- main
-      <LoginPageSignInSignUp />
-      <LoginPageFields />
-      <LoginPageOptions />
-      <LoginPageSendButton />
-      <LoginPageExternalAuthButtons />
-      <LoginPagefooter />
-    </div>
-  );
+  return <div>Yoad make the UI herer\</div>;
 }
