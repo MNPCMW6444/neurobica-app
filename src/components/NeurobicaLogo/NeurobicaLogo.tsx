13c24f2d6efb58d56daf8c2365ae4f0c0de718c5
YoadNatan <yoad12121223@gmail.com>
2022-08-17 22:17:37 +0300

    logo-fix

--- src/components/NeurobicaLogo/NeurobicaLogo.tsx
+++ src/components/NeurobicaLogo/NeurobicaLogo.tsx
@@ -3,18 +3,13 @@ import NeurobicaLogoAsset from "../../assets/LogoPNG.png";
 export default function NeurobicaLogo() {
   return (
     <Box
-      component="img"
-      sx={{
-        height: { xs: "4vh" },
-        width: { xs: "6vh" },
-        maxHeight: { xs: "60px" },
-        maxWidth: { xs: "150px" },
-        position: { xs: "fixed" },
-        top: { xs: 5 },
-        left: { xs: 10 },
-      }}
-      alt="Neurobica Logo"
-      src={NeurobicaLogoAsset}
-    />
+    component="img"
+    sx={{
+      maxWidth: { xs: "200px" },
+    }}
+    alt="Neurobica Logo"
+    src={NeurobicaLogoAsset}
+  />
+
   );
 }
--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -1,22 +1,18 @@
 import Button from "@mui/material/Button";
 import LoginStatus from "../LoginStatus/LoginStatus";
 import "./statusBar.css";
-import NeurobicaLogo from "../NeurobicaLogo/NeurobicaLogo";
+import NeurobicaLogo from "../NeurobicaSmallLogo/NeurobicaSmallLogo";
+import Grid from "@mui/material/Grid";
 
 export default function StatusBar() {
   return (
     <>
       <div className="statusBar">
-        <div
-          className="neurobicaLogo"
-          style={{
-            margin: "43%",
-          }}
-        >
+        <Grid container>
+          <Grid item >
           <NeurobicaLogo />
-        </div>
-        <div>
-          <LoginStatus />
+        </Grid>
+        <Grid item justifyContent="right ">
           <Button
             variant="outlined"
             href="../../my-account/auth"
@@ -27,7 +23,9 @@ export default function StatusBar() {
           >
             Login
           </Button>
-        </div>
+        </Grid>
+
+        </Grid>
       </div>
     </>
   );
