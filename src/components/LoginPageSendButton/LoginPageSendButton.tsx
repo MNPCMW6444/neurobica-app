d3c7cd9a025a48677d6370d9448f2d9f8c4527d3
YoadNatan <yoad12121223@gmail.com>
2022-08-17 20:53:13 +0300

    login button

--- src/components/LoginPageSendButton/LoginPageSendButton.tsx
+++ src/components/LoginPageSendButton/LoginPageSendButton.tsx
@@ -1,5 +1,18 @@
+import Button from "@mui/material/Button";
+import { borderColor } from "@mui/system";
 import React from "react";
 
 export default function LoginPageSendButton() {
-  return <div>LoginPageSendButton</div>;
+  return <div>
+    <Button color="inherit"
+            variant="outlined"
+            href="../../my-account/auth"
+            sx={{
+            color: "#FF8B17",
+            width:"45vh",
+            height:"6vh",
+            borderRadius:"30px"
+
+            }}>Login</Button>
+  </div>;
 }
