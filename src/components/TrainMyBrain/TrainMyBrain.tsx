f9a0caf0cdfe14f8407c3521059df342f8f072b1
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-21 16:36:36 +0300

    Update TrainMyBrain.tsx

--- src/components/TrainMyBrain/TrainMyBrain.tsx
+++ src/components/TrainMyBrain/TrainMyBrain.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import GoogleLogin from "react-google-login";
+import GoogleLogin from "@leecheuk/react-google-login";
 
 export default function TrainMyBrain() {
   const [awe, setAwe] = useState<string>();
@@ -7,6 +7,7 @@ export default function TrainMyBrain() {
   return (
     <>
       <GoogleLogin
+        style={{ position: "fixed", top: "200px", left: "100px" }}
         clientId="1004322422072-682m9pbt3rulm9qf0bjs1l41uqvk39n8.apps.googleusercontent.com"
         buttonText="Login"
         onSuccess={() => setAwe("Success")}
