f1932a9bfcda4ecaa837da3eec3b563dce2bd76f
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-21 16:29:14 +0300

    ok

--- src/components/TrainMyBrain/TrainMyBrain.tsx
+++ src/components/TrainMyBrain/TrainMyBrain.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import GoogleLogin from "react-google-login";
 
 export default function TrainMyBrain() {
   const [awe, setAwe] = useState<string>();
 
   return (
     <>
-      <GoogleLogin
+      {/*  <GoogleLogin
         clientId="1004322422072-682m9pbt3rulm9qf0bjs1l41uqvk39n8.apps.googleusercontent.com"
         buttonText="Login"
         onSuccess={() => setAwe("Success")}
         onFailure={() => setAwe("Falute")}
         cookiePolicy={"single_host_origin"}
-      />
+      /> */}
       <p>{awe}</p>
     </>
   );
