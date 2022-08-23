1fa0292072e55eeb1b8f212e71874914e4e12aa0
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-23 12:57:14 +0300

    Revert "Revert "Merge branch 'yoad-main' into yoad-ui-main""
    This reverts commit 4831ee6fc5cd2e437b8a2417a8706843821ca55c.

--- src/components/LoginPageSendButton/LoginPageSendButton.tsx
+++ src/components/LoginPageSendButton/LoginPageSendButton.tsx
@@ -2,12 +2,18 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 import Signin from "../Signin/Signin";
 
-export default function LoginPageSendButton(props: {
+interface LoginPageSendButtonProps {
   isAllowedToSignIn: boolean;
   email: string;
   password: string;
 }) {
   const [label, setLabel] = useState<string>("Continue");
+=======
+  isSignUp: boolean;
+}
+
+export default function LoginPageSendButton(props: LoginPageSendButtonProps) {
+  const [label, setLabel] = useState<string>("Sign In");
 
   return (
     <>
@@ -21,11 +27,20 @@ export default function LoginPageSendButton(props: {
           height: "6vh",
           borderRadius: "30px",
         }}
-        onClick={() => setLabel("Signing in...")}
+        onClick={() =>
+          props.isSignUp ? setLabel("Signing up...") : setLabel("Signing in...")
+        }
       >
         {label}
       </Button>
       {label === "Signing in..." && (
+        <Signin
+          setLabel={setLabel}
+          email={props.email}
+          password={props.password}
+        />
+      )}
+      {label === "Signing up..." && (
         <Signin
           setLabel={setLabel}
           email={props.email}
