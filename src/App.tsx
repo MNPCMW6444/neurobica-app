27d97c2b98c20a83e58103c04aead317e4cc3e8f
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-23 20:12:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Home from "./components/Home/Home";
 import MyAccount from "./components/MyAccount/MyAccount";
 import MyBrain from "./components/MyBrain/MyBrain";
 import TrainMyBrain from "./components/TrainMyBrain/TrainMyBrain";
-import LoginPage from "./components/LoginPage/LoginPage";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
@@ -19,79 +18,85 @@ Axios.defaults.withCredentials = true;
 
 function App() {
   const [value, setValue] = useState<string>();
-  const [finished, setFinished] = useState<boolean>(false);
-  const [result, setResult] = useState<boolean>(false);
+  const [status, setStatus] = useState<string>(
+    "Checking server availability..."
+  );
   useEffect(() => {
     const checkIfServerIsThere = async () => {
-      const ttt = (await Axios.get(domain + "areyoualive")).data;
-      setResult(ttt.answer === "yes");
-      setFinished(true);
-    }
-    checkIfServerIsThere();
-  }, [])
-  setTimeout(() => { if (!finished) { setFinished(true); setResult(false); } }, 5000);
+      setStatus(
+        (await Axios.get(domain + "areyoualive")).data.aswer === "yes"
+          ? "good"
+          : "bad" + Math.random()
+      );
+    };
+    status === "Checking server availability..."
+      ? checkIfServerIsThere()
+      : setTimeout(() => checkIfServerIsThere(), 5000);
+  }, [status]);
 
-  return (
-    finished ? result ?
-      (<div style={{ height: "100vh", width: "100vw" }
-      } >
-        <div
-          style={{
-            height: "5%",
-            width: "100%",
-            position: "fixed",
-            top: "0px",
-          }}
-        >
-          <StatusBar />
-        </div>
-        <div
-          style={{
-            height: "85%",
-            width: "100%",
-            overflow: "scroll",
-          }}
-        >
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/my-account" element={<MyAccount />} />
-              <Route path="/my-brain" element={<MyBrain />} />
-              <Route path="/train-my-brain" element={<TrainMyBrain />} />
-              <Route path="/termsvf" element={<TermsAndConditions />} />
-            </Routes>
-          </Router>
-        </div>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(value, newValue) => {
-            setValue(newValue);
-          }}
-          sx={{
-            backgroundColor: "#F5F5F5",
-            height: "10%",
-            position: "fixed",
-            bottom: "0",
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <BottomNavigationAction
-            href="/train-my-brain"
-            label="Train"
-            icon={<FitnessCenterIcon />}
-          />
-          <BottomNavigationAction href="/home" label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction
-            href="/my-brain"
-            label="Brain"
-            icon={<PsychologyOutlinedIcon />}
-          />
-        </BottomNavigation>
-      </div >) : (<div>fuck you</div>) : (<div>loading...</div>)
+  return status === "Checking server availability..." ? (
+    <p>Checking server availability...</p>
+  ) : status === "good" ? (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <div
+        style={{
+          height: "5%",
+          width: "100%",
+          position: "fixed",
+          top: "0px",
+        }}
+      >
+        <StatusBar />
+      </div>
+      <div
+        style={{
+          height: "85%",
+          width: "100%",
+          overflow: "scroll",
+        }}
+      >
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/my-account" element={<MyAccount />} />
+            <Route path="/my-brain" element={<MyBrain />} />
+            <Route path="/train-my-brain" element={<TrainMyBrain />} />
+            <Route path="/termsvf" element={<TermsAndConditions />} />
+          </Routes>
+        </Router>
+      </div>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(value, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{
+          backgroundColor: "#F5F5F5",
+          height: "10%",
+          position: "fixed",
+          bottom: "0",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <BottomNavigationAction
+          href="/train-my-brain"
+          label="Train"
+          icon={<FitnessCenterIcon />}
+        />
+        <BottomNavigationAction href="/home" label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          href="/my-brain"
+          label="Brain"
+          icon={<PsychologyOutlinedIcon />}
+        />
+      </BottomNavigation>
+    </div>
+  ) : (
+    <p>Sorry, yoad make this message</p>
   );
 }
 
