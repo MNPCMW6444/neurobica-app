dc408d1991375d3f0136964ab563423f4654735a
YoadNatan <yoad12121223@gmail.com>
2022-08-24 13:29:48 +0300

    home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,13 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 export default function Home() {
   return (
-    <div className="homePage">
-      wekjfdnjkwefjkwe fjkwe fjwe jkf wejkf jkwe fjkwe fjkw fj wefk wejkf wejkf
-      jkwe
-      <p>sedwed</p>
-      <p>sedwed</p>
-      <p>sedwed</p>
-      <p>sedwed</p>
-      fjkwe fjw jkf wj,ef wjkHome Page
-    </div>
+    <Grid>
+      <Typography variant="h2" gutterBottom>
+        h2. Heading
+      </Typography>
+    </Grid>
+
   );
 }
