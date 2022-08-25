dfc5bc3362607b78ed31e14625c2587cae2dcc06
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-25 12:12:28 +0300

    refactoring

--- src/components/UserVisualData/UserVisualData.tsx
+++ src/components/UserVisualData/UserVisualData.tsx
@@ -1,40 +1,5 @@
-import Paper from "@mui/material/Paper";
-import {
-    Chart,
-    ArgumentAxis,
-    ValueAxis,
-    BarSeries
-} from "@devexpress/dx-react-chart-material-ui";
+import React from "react";
 
-import { scaleBand } from "@devexpress/dx-chart-core";
-import { ArgumentScale, Stack } from "@devexpress/dx-react-chart";
-
-import userbrain from "../../../temp/DemoChart/Chart";
-import { useState } from "react";
-
-export default () => {
-
-
-
-    return (
-        <Paper>
-            {/*  <Chart data={[{ data: userbrain }]}>
-                <ArgumentScale factory={scaleBand} />
-                <ArgumentAxis />
-                <ValueAxis />
-
-                <BarSeries
-                    valueField="Attention"
-                    argumentField="state"
-                    name="Attention"
-                />
-                <BarSeries valueField="Memory" argumentField="state" name="Memory" />
-                <BarSeries valueField="Memory" argumentField="state" name="Memory" />
-                <Stack />
-            </Chart> */}
-        </Paper >
-    );
-
+export default function UserVisualData() {
+  return <div>UserVisualData</div>;
 }
-
-
