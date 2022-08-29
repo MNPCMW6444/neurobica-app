3475f5608ee6857c160ca92742d11f046cc527f6
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-29 16:16:14 +0300

    asd

--- src/util/domain.ts
+++ src/util/domain.ts
@@ -1,13 +1,3 @@
-const domains: {
-  production: string;
-  preProduction: string;
-  local: string;
-} = {
-  production: "/server/",
-  preProduction: "https://neuronbica-admin.herokuapp.com/",
-  local: "http://localhost:6444/",
-};
-
 export default process.env.NODE_ENV === "production"
-  ? domains.production
-  : domains.local;
+  ? "/server/"
+  : "http://localhost:6444/";
