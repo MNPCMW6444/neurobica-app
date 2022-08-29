806281c5644e51753a8a4dc77dbc7a5557b7ae19
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-29 12:22:48 +0300

    Revert "FROM UBUNTU KASUM"
    This reverts commit 8a098f003a9fb2180e4ac656f2f37d5d84ce3adf.

--- server/index.js
+++ server/index.js
@@ -54,18 +54,13 @@ connectToDBs();
 app.use((0, heroku_ssl_redirect_1.default)());
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: process.env.NODE_ENV === "development"
-        ? [
-            "http://localhost:3000",
-            "https://app.neurobica.online",
-            "https://yoad-main-ui.flexboxtorchy.com",
-            "https://michael-app.flexboxtorchy.com",
-        ]
-        : [
-            "https://app.neurobica.online",
-            "https://yoad-main-ui.flexboxtorchy.com",
-            "https://michael-app.flexboxtorchy.com",
-        ],
+    origin: [
+        "http://localhost:3000",
+        "https://localhost:3000",
+        "https://app.neurobica.online",
+        "https://yoad-main-ui.flexboxtorchy.com",
+        "https://michael-app.flexboxtorchy.com",
+    ],
     credentials: true,
 }));
 app.use((0, cookie_parser_1.default)());
